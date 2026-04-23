ecaee1a68b545d22b4dd67fb360ea6e26ce5d5ad
smalldayDC_ <152234768+smalldayDC@users.noreply.github.com>
2026-04-23 20:11:06 +0800

    Fix missing CallbackInfo import

--- src/main/java/com/smalldaydc/friendlycreeper/mixin/MixinMobEntity.java
+++ src/main/java/com/smalldaydc/friendlycreeper/mixin/MixinMobEntity.java
@@ -1,12 +1,9 @@
 package com.smalldaydc.friendlycreeper.mixin;
 
-import com.smalldaydc.friendlycreeper.FriendlyCreeperConfig;
 import com.smalldaydc.friendlycreeper.ITamedCreeper;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.SnowGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -21,14 +18,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEntity.class)
 public class MixinMobEntity {
-
-    @Inject(method = "setTarget", at = @At("HEAD"), cancellable = true)
-    private void friendlycreeper$preventSnowGolemTargeting(LivingEntity target, CallbackInfo ci) {
-        if (!((Object) this instanceof SnowGolemEntity)) return;
-        if (FriendlyCreeperConfig.get().snowGolemAttack) return;
-        if (!(target instanceof CreeperEntity creeper)) return;
-        if (((ITamedCreeper)(Object) creeper).friendlycreeper$isTamed()) ci.cancel();
-    }
 
     @Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
     private void friendlycreeper$onInteract(PlayerEntity player, Hand hand,
